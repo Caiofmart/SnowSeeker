6355442605c483e014314b0e82064056237588e9
Caio <caiofmart@gmail.com>
2022-01-31 22:24:38 -0300

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Caio Feitoza Martins on 08/11/21.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna, Arya and Toby")
+        }
+    }
+}
+
+extension View {
+    func phoneOnlyStackNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return AnyView(self.navigationViewStyle(.stack))
+        } else {
+            return AnyView(self)
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var favorites = Favorites()
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    var body: some View {
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(destination: ResortView(resort: resort)) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                    .layoutPriority(1)
+                    
+                    if self.favorites.contains(resort) {
+                        Spacer()
+                        Image(systemName: "heart.fill")
+                        .accessibility(label: Text("This is a favorite resort"))
+                            .foregroundColor(.red)
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+            .phoneOnlyStackNavigationView()
+            
+            WelcomeView()
+        }
+        .environmentObject(favorites)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Caio Feitoza Martins on 08/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
